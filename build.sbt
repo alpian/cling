e92a908ebd8e826d037bfd9194f1916444fdf892
Ian Davies <id451@hotmail.com>
2015-08-25 00:46:51 +0200

    Ian: basic async undertow code

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
   "org.eclipse.jetty" % "jetty-server" % "9.3.2.v20150730",
   "io.undertow" % "undertow-core" % "1.3.0.Beta9",
+  "ch.qos.logback" % "logback-classic" % "1.1.3",
   "com.typesafe" % "config" % "1.3.0")
 
 // Uncomment to use Akka
--- src/main/scala/com/ind/cling/Launch.scala
+++ src/main/scala/com/ind/cling/Launch.scala
@@ -1,16 +1,36 @@
 package com.ind.cling
 
 import com.typesafe.config.ConfigFactory
+import io.undertow.Undertow
+import io.undertow.server.{HttpServerExchange, HttpHandler}
+import io.undertow.util.Headers
+import org.slf4j.{LoggerFactory, Logger}
 
 class HttpServer(port: Int) {
   def start() = {
+    val server = Undertow.builder()
+      .addHttpListener(port, "localhost")
+      .setHandler(new HttpHandler() {
+      def handleRequest(exchange: HttpServerExchange) = {
+        if (exchange.isInIoThread) {
+          exchange.dispatch(this)
+        }
+        exchange.getResponseHeaders.put(Headers.CONTENT_TYPE, "text/plain")
+        exchange.getResponseSender.send("Hello World2")
+      }
+    }).build()
+    server.start()
   }
 }
 
 object Launch {
   def main(args: Array[String]): Unit = {
+    System.setProperty("org.jboss.logging.provider", "slf4j")
+    System.setProperty("cling.http.server.port", "8080")
     val port = ConfigFactory.load().getInt("cling.http.server.port")
-    println(s"Cling server starting on port $port...")
+    val logger = LoggerFactory.getLogger(classOf[HttpServer])
+    logger.info(s"Cling server is starting on port $port...")
     new HttpServer(port).start()
+    logger.info(s"Cling server listening on port $port")
   }
 }
